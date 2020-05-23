1b56286a1328d29c633b6671a08ca945bc98a3fc
Elias Porcio <gggggfff@web.de>
2020-05-23 15:47:08 +0200

    task5: Added conversion to plaintext, lemmatisation and stopword removal

--- task5/WikipediaAnalyzer/build.sbt
+++ task5/WikipediaAnalyzer/build.sbt
@@ -4,7 +4,8 @@ scalaVersion := "2.12.0"
 val sparkVersion = "2.4.5"
 
 resolvers ++= Seq(
-  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+  "apache-snapshots" at "http://repository.apache.org/snapshots/",
+  "jitpack" at "https://jitpack.io"
 )
 
 libraryDependencies ++= Seq(
@@ -12,4 +13,6 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % sparkVersion,
   "com.databricks" % "spark-xml_2.12" % "0.9.0",
   "edu.stanford.nlp" % "stanford-corenlp" % "4.0.0",
+  "edu.stanford.nlp" % "stanford-corenlp" % "4.0.0" classifier "models",
+  "org.wikiclean" % "wikiclean" % "1.2"
 )
--- task5/WikipediaAnalyzer/src/main/scala/WikipediaAnalyzer.scala
+++ task5/WikipediaAnalyzer/src/main/scala/WikipediaAnalyzer.scala
@@ -1,6 +1,11 @@
 import com.databricks.spark.xml._
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
+import java.util.Properties
+import edu.stanford.nlp.pipeline.CoreDocument
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import org.wikiclean.WikiClean
+import collection.JavaConverters._
 
 object WikipediaAnalyzer {
   def main(args: Array[String]): Unit = {
@@ -12,21 +17,74 @@ object WikipediaAnalyzer {
       .option("rowTag", "page")
       .xml("enwiki_small.xml")
 
-    //filter redirect entries
+    //Remove entries that only redirect to actual articles
     val noRedirects = dataframe.filter(row => row.getAs("redirect") == null)
 
+    //Create a pair RDD of article titles to article text
     //noRedirects is of the type Dataframe. The corresponding RDD is required so it can be turned into a pair RDD using the .map call
     val keyValue = noRedirects.rdd.map(row =>
       (
         row.getAs("title").toString(), //key
-        row.getAs("revision").asInstanceOf[sql.Row].getAs("text").toString()) //value
+        row.getAs("revision") //value
+          .asInstanceOf[sql.Row]
+          .getAs("text")
+          .asInstanceOf[sql.Row]
+          .getAs("_VALUE")
+          .toString()
       )
+    )
 
-    println(keyValue.lookup("Bacteriophage"))
+    //Convert the Wiki markup text to actual plaintext
+    val plainText = keyValue.mapValues(Functions.toPlaintext)
+
+    //Remove external links in square brackets
+    val noLinks = plainText.mapValues(x => x.replaceAll("\\[.*?\\]", ""))
+
+    //Change uppercase to lowercase letters and remove everything that is not a letter or whitespace
+    val onlyLowercaseLetters = noLinks.mapValues(x => x.toLowerCase().replaceAll("[^a-z|\\s]", ""))
+
+    //Split article into words (Actual lemmatisation need performance optimization)
+    val lemmas = onlyLowercaseLetters.flatMapValues(Functions.toLemmas)
+
+    //Remove unnecessary frequently used words
+    val noStopwords = lemmas.filter(pair => !StopWords.list.contains(pair._2))
+
+    noStopwords.lookup("Bacteriophage").foreach(println)
+
     try {
       spark.stop()
     } catch {
       case _: Throwable => () //Exceptions are expected here
     }
+  }
+}
+
+object Functions {
+  val wikiCleaner = new WikiClean.Builder().build
+  val nlpProperties = new Properties()
+  //nlpProperties.setProperty("annotators", "tokenize, ssplit, pos, lemma")
+  nlpProperties.setProperty("annotators", "tokenize")
+  val nlp = new StanfordCoreNLP(nlpProperties)
+
+  /* Converts Wiki markup into plaintext */
+  def toPlaintext(text: String): String = {
+    //The xml tags had been removed as the file was loaded into a dataframe,
+    //but are expected by the library
+    return wikiCleaner.clean("<text xml:space=\"preserve\">" + text + "</text>")
+  }
+
+  /* Currently only splits Strings into words. Actual lemmatisation takes a very long time.
+  *  Further optimization is needed.
+  */
+  def toLemmas(text: String): List[String] = {
+
+    val doc = new CoreDocument(text)
+    nlp.annotate(doc)
+/*
+    val tokens = doc.tokens().asScala
+    val lemmas = tokens.map(x => x.lemma()).toList
+    return lemmas
+*/
+    return doc.tokens().asScala.toList.map(x => x.toString())
   }
 }
